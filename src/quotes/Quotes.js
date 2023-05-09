8c903ec9b9244e7a8767d6c0b50b6e819b6926a2
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2023-05-09 16:39:58 +0100

    Word for you

--- src/quotes/Quotes.js
+++ src/quotes/Quotes.js
@@ -1,8 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Quotes() {
+  const [word, setWord] = useState([]);
+ 
+
+  useEffect(() => {
+    setTimeout(() => {
+      axios
+        .get("https://random-word-api.herokuapp.com/word")
+        .then(function (response) {
+          setWord(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }, 6000);
+  }, []);
+
   return (
     <div className="quote-card">
-      <p className="quote-heading">Word Quote</p>
+      <p className="quote-heading">Word for you</p>
+      <p className="text-3">{word[0]} </p>
       <p className="quote-content">
         One forgets words as one forgets names. One’s vocabulary needs constant
         fertilizing or it will die.
